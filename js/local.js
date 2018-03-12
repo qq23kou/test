291d10e44a95f8991eb687d810f98c18ff6ad098
guosongyang <guosongyang@52yunxi.cn>
2018-03-12 15:11:09 +0800

    Signed-off-by: guosongyang <guosongyang@52yunxi.cn>

--- js/local.js
+++
@@ -1,7 +0,0 @@
-    document.getElementById("Name").innerHTML = localStorage.getItem("name");
-    var num1 = parseInt(10*Math.random());
-    var num2 = parseInt(10*Math.random());
-    var num3 = parseInt(10*Math.random());
-    var num4 = parseInt(10*Math.random());
-    var num5 = parseInt(10*Math.random());
-    document.getElementById("Name_nb").innerHTML = num1+""+num2+""+num3+""+num4+""+num5;
